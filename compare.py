aa88d298211c4d6b254a865297a186ecc52a28bf
daniel kurushin <daniel.kurushin@gmail.com>
2019-04-26 13:28:50 +0500

    test

--- compare.py
+++ compare.py
@@ -1,8 +1,19 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
 Created on Tue Apr 23 13:08:33 2019
 
 @author: dan
 """
 
+from PIL import Image
+
+def compare(img1, img2):
+    img1 = Image.open(img1)
+    img2 = Image.open(img2)
+    return 1
+
+if __name__ == '__main__':
+    test= ['test/img1.png', 'test/img2.png']
+    for img1 in test:
+        for img2 in test:
+            print("%s <=> %s (%s)" % (img1, img2, compare(img1, img2)))
+    
