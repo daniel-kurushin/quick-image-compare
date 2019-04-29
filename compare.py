cd6034298c199d6a33d76d74637d118d11affd30
daniel.kurushin@gmail.com <daniel.kurushin@gmail.com>
2019-04-29 22:51:03 +0500

    neuro, gitignore

--- compare.py
+++ compare.py
@@ -6,34 +6,93 @@ Created on Tue Apr 23 13:08:33 2019
 
 from PIL import Image, ImageOps
 from copy import deepcopy
+import numpy as np
 
-SIZE = 24
+SIZE = 8
 e = 2.78
-W = [[0]*24]*24
+W = np.zeros((SIZE,SIZE))
+a = 0.5
+IMG = {}
+epoc = 0
+
+def plot(w):
+    global epoc
+    
+    from matplotlib import pyplot as plt
+    ww = np.zeros((SIZE*20,SIZE*20))
+    for x in range(SIZE * 20):
+        for y in range(SIZE * 20):
+            ww[x,y] = w[x//20,y//20]
+    plt.imshow(ww)
+    plt.savefig("%s" % epoc)
+    epoc += 1
+
+def get_image(img):
+    global IMG
+
+    try:
+        return IMG[img]
+    except KeyError:
+        data = ImageOps.grayscale(Image.open(img).resize(size = (SIZE,SIZE), resample = Image.HAMMING))
+        IMG.update({img:data})
+        return data
+        
+def train():
+    global W
+
+    from os import listdir
+    from os.path import isfile, join
+    from json import dumps
+    images = [ join('test', f) for f in listdir('test') if isfile(join('test', f))]
+
+    Δ = [1] * len(images) * len(images)
+    while sum(Δ) > 3:
+        i = 0
+        for img1 in images:
+            class1 = img1.split('.')[0]
+            for img2 in images:
+                class2 = img2.split('.')[0]
+                y = 0 if class1 == class2 else 1
+                W = deepcopy(update_w(img1, img2, y))
+                r = compare(img1, img2)
+                Δ[i] = abs(y - r)
+                print(img1, img2, y, r)
+                i += 1
+        plot(W)
+        print("sum = " % sum(Δ))
+
+
+def update_w(img1, img2, y):
+    w = deepcopy(W)
+    for x in range(SIZE):
+        for y in range(SIZE):
+            w[x,y] += a * (y - compare(img1, img2)) * δ(img1, img2, x, y)
+    return w
+
 
 def σ(x):
     return 1 / (1 + e**(-x))
 
-def train(img1, img2, y):
-    w = deepcopy(W)
-    for x in range(SIZE):
-        for y in range(SIZE):
-            w[x][y] += a * (compare(img1, img2) - y)'
-    return w
+
+def δ(img1, img2, x, y):
+    img1 = get_image(img1)
+    img2 = get_image(img2)
+    a = σ(img1.getpixel((x,y)) / 255)
+    b = σ(img2.getpixel((x,y)) / 255)
+    return (a-b)**2
+
 
 def compare(img1, img2):
-    img1 = ImageOps.grayscale(Image.open(img1).resize(size = (SIZE,SIZE), resample = Image.HAMMING))
-    img2 = ImageOps.grayscale(Image.open(img2).resize(size = (SIZE,SIZE), resample = Image.HAMMING))
     D = []
     for x in range(SIZE):
         for y in range(SIZE):
-            a = σ(img1.getpixel((x,y)) / 255)
-            b = σ(img2.getpixel((x,y)) / 255)
-            D += [(a-b)**2] * W[x][y]
+            v = δ(img1, img2, x, y)
+            D += [v * W[x,y]]
 #    print(D)
-    return sum(D)**1/2
+    return σ(sum(D)**1/2)-1/2
 
-if __name__ == '__main__':
+
+def test():
     from os import listdir
     from os.path import isfile, join
     from json import dumps
@@ -55,3 +114,8 @@ if __name__ == '__main__':
                 }]
     resylt = sorted(resylt, key=lambda x: x['q'])
     print (dumps(resylt, ensure_ascii=0, indent=2))
+
+
+if __name__ == '__main__':
+    train()
+    test()
